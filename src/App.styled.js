0bbbb2fb885f947a076ac06042d271ce8fc2d93f
YaremaNatalia <yaremka.n@gmail.com>
2023-09-26 21:29:34 +0300

    fix top color

--- src/App.styled.js
+++ src/App.styled.js
@@ -46,7 +46,7 @@ export const AppNav = styled.nav`
   justify-content: space-between;
   align-items: center;
   margin-bottom: 16px;
-  background-color: #1677ff;
+  background-color: #bc8777;
   box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 4px -1px,
     rgba(0, 0, 0, 0.14) 0px 4px 5px 0px, rgba(0, 0, 0, 0.12) 0px 1px 10px 0px;
   /* border-bottom: 1px solid #000; */
